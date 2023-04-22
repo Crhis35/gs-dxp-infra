b22a5f69a239e70b3ffb50bbd0659a461eda62cb
Crhistian Caraballo <cristianjcj35@gmail.com>
2023-04-22 15:58:15 -0500

    Upadted jenkins script

--- modules/ec2_instance/scripts/jenkinsInstallation.sh
+++ modules/ec2_instance/scripts/jenkinsInstallation.sh
@@ -9,9 +9,10 @@ sudo service docker start
 
 #Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 sudo amazon-linux-extras install java-openjdk11 -y
+sudo amazon-linux-extras install fontconfig -y
 
 sudo yum install jenkins -y
 
